2c5c45ff710cc4f25d653f19a8c71126809d4e92
Kirninn <44545518+Kirninn@users.noreply.github.com>
2019-02-17 20:32:56 +0500

    Update Hero.py

--- Hero.py
+++ Hero.py
@@ -76,8 +76,13 @@ class Blessing(AbstractPositive):
     
     def get_stats(self):
         stats = self.base.get_stats().copy()
-        for a, b in stats.items():
-            stats[a] = b + 2
+        stats['Strength'] += 2
+        stats['Endurance'] += 2
+        stats['Agility'] += 2
+        stats['Luck'] += 2
+        stats['Intelligence'] += 2
+        stats['Perception'] += 2
+        stats['Charisma'] += 2
         return stats
 
 
@@ -95,8 +100,13 @@ class Curse(AbstractNegative):
 
     def get_stats(self):
         stats = self.base.get_stats().copy()
-        for a, b in stats.items():
-            stats[a] = b - 2
+        stats['Strength'] -= 2
+        stats['Endurance'] -= 2
+        stats['Agility'] -= 2
+        stats['Luck'] -= 2
+        stats['Intelligence'] -= 2
+        stats['Perception'] -= 2
+        stats['Charisma'] -= 2
         return stats
 
 

7024af2612cfad0fe73053a739658dbaff883c61
Kirninn <chugunovdn45@gmail.com>
2019-03-02 13:27:17 +0500

    add adapter

--- Adapter.py
+++ Adapter.py
@@ -28,5 +28,19 @@ class System:
     
     def get_lightening(self, light_mapper):
         self.lightmap = light_mapper.lighten(self.map)
-    
-    ...
+
+class MappingAdapter:
+    def __init__(self, adaptee):
+        self.adaptee = adaptee
+
+    def lighten(self, grid):
+        self.adaptee.set_dim((len(grid[0]), len(grid)))
+        light = [(i2, i1) for i1, e1 in enumerate(grid) for i2, e2 in enumerate(e1) if e2 == 1]
+        obstacles = [(i2, i1) for i1, e1 in enumerate(grid) for i2, e2 in enumerate(e1) if e2 == -1]
+        print(light)
+        print(obstacles)
+        self.adaptee.set_lights(light)
+        self.adaptee.set_obstacles(obstacles)
+        
+        return self.adaptee.generate_lights()
+

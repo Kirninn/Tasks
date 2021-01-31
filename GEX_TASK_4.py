f349250bde4b27189e50c89653503b386eb9441d
Denis <44545518+Kirninn@users.noreply.github.com>
2021-01-31 10:45:50 +0500

    Update GEX_TASK_4.py

--- GEX_TASK_4.py
+++ GEX_TASK_4.py
@@ -12,12 +12,9 @@ HEADERS = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/5
 
 html_for_parse = requests.get(URL, headers = HEADERS)
 
-os.mkdir(r'C:\Users\Kirninn\Work\tutorial\Astronomy Picture of the Day Archive')
-
 date_articles_last_year = []
 
 def get_count(html):
-    global date_articles_last_year
     soup = BeautifulSoup(html.text, 'html.parser')
     items = str(soup.body.find_all('b')[1]).replace('\n', '').replace('</a>', '').replace('</b>', '').replace('<b>', '').strip().split('<br/>')
     items.remove('')
@@ -33,40 +30,48 @@ def get_count(html):
     year_find = datetime.now() - timedelta(days=365)
 
     for keys in date_articles:
-        if keys['date_art'] >= year_find:
-            keys['date_art'] = keys['date_art'].strftime('%Y-%m-%d')
-            date_articles_last_year.append(keys)
+        url = 'https://apod.nasa.gov/apod/' + (''.join(keys['link_art']))
+        requests_have_image = requests.get(url)
+        soup_have_image = BeautifulSoup(requests_have_image.text, 'html.parser')
+        search = soup_have_image.find('img')
+        if (search is not None):
+            if keys['date_art'] >= year_find:
+                keys['date_art'] = keys['date_art'].strftime('%Y-%m-%d')
+                date_articles_last_year.append(keys)
+            else:
+                break
 
 get_count(html_for_parse)
 
-for item in date_articles_last_year:
-    root = r'C:\Users\Kirninn\Work\tutorial\Astronomy Picture of the Day Archive'
-    dir_path = os.path.join(root, item['date_art'])
-    os.makedirs(dir_path)
-    file_path = os.path.join(dir_path, f"{item['date_art']}.txt")
+def get_text_and_image():
 
-    url = 'https://apod.nasa.gov/apod/' + (''.join(item['link_art']))
-    requests_print = requests.get(url)
+    os.mkdir(r'C:\Users\Kirninn\Work\tutorial\Astronomy Picture of the Day Archive')
 
-    soup = BeautifulSoup(requests_print.text, 'html.parser')
-    text_article = str(soup.body.find_all('p')[2].get_text()).strip().split("Tomorrow's")
+    for item in date_articles_last_year:
+        root = r'C:\Users\Kirninn\Work\tutorial\Astronomy Picture of the Day Archive'
+        dir_path = os.path.join(root, item['date_art'])
+        os.makedirs(dir_path)
+        file_path = os.path.join(dir_path, f"{item['date_art']}.txt")
 
-    list_article = []
+        url = 'https://apod.nasa.gov/apod/' + (''.join(item['link_art']))
+        requests_print = requests.get(url)
 
-    for item in text_article:
-        list_article.append(item)
+        soup = BeautifulSoup(requests_print.text, 'html.parser')
+        text_article = str(soup.body.find_all('p')[2].get_text()).strip().split("Tomorrow's")
 
-    with open(file_path, 'w', encoding='utf-8') as file:
-        file.write(list_article[0])
+        list_article = []
 
-    search = soup.find('img')
-    if (search is None):
-        continue
-    else:
+        for item in text_article:
+            list_article.append(item)
+
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write(list_article[0])
+
         img_src = soup.find('img')['src']
-        url_image = 'https://apod.nasa.gov/apod/' + img_src
 
-        response_image = requests.get(url_image)
+        response_image = requests.get('https://apod.nasa.gov/apod/' + img_src)
 
         with open(os.path.join(dir_path, 'image_of_day.jpg'), 'wb') as imgfile:
             imgfile.write(response_image.content)
+
+get_text_and_image()

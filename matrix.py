0c49061084f7af6b00d1e333e5f5347c4b0d1474
Denis <44545518+Kirninn@users.noreply.github.com>
2021-08-23 14:53:11 +0500

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,24 +1,16 @@
 class Matrix:
 
     def __init__(self, matrix_string):
-        self.matrix_string = matrix_string
-        self.matrix_string = [list(line) for line in self.matrix_string.splitlines()]
+        self.matrix_string = [line.split() for line in matrix_string.split('\n')]
 
     def row(self, index):
         row_list = []
         for item in self.matrix_string[index - 1]:
-            if item == ' ':
-                pass
-            else:
-                row_list += item
+            row_list.append(item)
         return list(map(int, row_list))
     
     def column(self, index):
         column_list = []
         for item in self.matrix_string:
-            column_list += item[index - 1][index - 1]
+            column_list.append(item[index - 1])
         return list(map(int, column_list))
-            
-c = Matrix('10 20 30\n4 5 6')
-
-print(c.row(1))
